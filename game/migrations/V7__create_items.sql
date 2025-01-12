d1c9c64cec452101b58d4327a42ba16bf2c15418
lucasdray <pedrolucasdourado@hotmail.com>
2025-01-12 15:58:31 -0300

    create items

---
+++ game/migrations/V7__create_items.sql
@@ -0,0 +1,79 @@
+CREATE TABLE Tipo_Item (
+    id_item INTEGER PRIMARY KEY,
+    tipo_item INTEGER NOT NULL
+);
+
+CREATE TABLE Armadura (
+    id_armadura INTEGER,
+    id_parte_corpo enum_parte_corpo,
+    nome VARCHAR NOT NULL,
+    raridade_armadura VARCHAR NOT NULL,
+    defesa_magica INTEGER,
+    defesa_fisica INTEGER,
+    ataque_magico INTEGER,
+    ataque_fisico INTEGER,
+    durabilidade_max INTEGER,
+    preco_venda INTEGER,
+    descricao INTEGER,
+    PRIMARY KEY (id_armadura, id_parte_corpo)
+);
+ 
+CREATE TABLE Material (
+    id_material INTEGER PRIMARY KEY,
+    nome VARCHAR,
+    preco_venda INTEGER,
+    descricao VARCHAR
+);
+ 
+CREATE TABLE Item_Missao (
+    id_item INTEGER PRIMARY KEY,
+    nome VARCHAR,
+    descricao VARCHAR
+);
+ 
+CREATE TABLE Consumivel (
+    id_item INTEGER PRIMARY KEY,
+    nome VARCHAR,
+    descricao VARCHAR,
+    preco_venda INTEGER,
+    saude_restaurada INTEGER,
+    magia_restaurada INTEGER,
+    saude_maxima INTEGER,
+    magia_maxima INTEGER
+);
+
+CREATE TABLE Livro (
+    id_item INTEGER PRIMARY KEY,
+    id_habilidade INTEGER,
+    nome VARCHAR,
+    descricao VARCHAR,
+    preco_venda INTEGER
+);
+
+ALTER TABLE Armadura ADD CONSTRAINT FK_Armadura_2
+    FOREIGN KEY (id_armadura)
+    REFERENCES Tipo_Item (id_item);
+ 
+ALTER TABLE Armadura ADD CONSTRAINT FK_Armadura_3
+    FOREIGN KEY (id_parte_corpo)
+    REFERENCES Parte_Corpo (id_parte_corpo);
+
+ALTER TABLE Material ADD CONSTRAINT FK_Material_2
+    FOREIGN KEY (id_material)
+    REFERENCES Tipo_Item (id_item);
+
+ALTER TABLE Item_Missao ADD CONSTRAINT FK_Item_Missao_2
+    FOREIGN KEY (id_item)
+    REFERENCES Tipo_Item (id_item);
+
+ALTER TABLE Livro ADD CONSTRAINT FK_Livro_2
+    FOREIGN KEY (id_item)
+    REFERENCES Tipo_Item (id_item);
+ 
+ALTER TABLE Livro ADD CONSTRAINT FK_Livro_3
+    FOREIGN KEY (id_habilidade)
+    REFERENCES Habilidade (id_habilidade);
+
+ALTER TABLE Consumivel ADD CONSTRAINT FK_Consumivel_2
+    FOREIGN KEY (id_item)
+    REFERENCES Tipo_Item (id_item);
