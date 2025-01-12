2570551bd26c23ea83bd6b4c0543dc73c1e9b3e7
unknown <180145509@aluno.unb.br>
2025-01-12 19:18:12 -0300

    corrige nome e ordem dos arquivos

--- game/migrations/V16__create_inventario.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Inventario (
-    id_player INTEGER PRIMARY KEY,
-    dinheiro INTEGER NOT NULL
-);
- 
-ALTER TABLE Inventario ADD CONSTRAINT FK_Inventario_1
-    FOREIGN KEY (id_player)
-    REFERENCES Player (id_player);
