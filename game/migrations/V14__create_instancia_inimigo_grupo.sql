4cc6092776b15562aed598c84a578dc78efb1242
lucasdray <pedrolucasdourado@hotmail.com>
2025-01-12 16:41:36 -0300

    create instancia e grupo inimigo

---
+++ game/migrations/V14__create_instancia_inimigo_grupo.sql
@@ -0,0 +1,28 @@
+CREATE TABLE Grupo_inimigo (
+    id_grupo INTEGER PRIMARY KEY,
+    id_sala INTEGER
+);
+ 
+
+CREATE TABLE Instancia_Inimigo (
+    id_inimigo INTEGER,
+    id_instancia INTEGER,
+    id_grupo INTEGER,
+    hp_atual INTEGER NOT NULL,
+    magia_atual INTEGER NOT NULL,
+    defesa_fisica_bonus INTEGER,
+    defesa_magica_bonus INTEGER,
+    PRIMARY KEY (id_inimigo, id_instancia)
+);
+ 
+ALTER TABLE Instancia_Inimigo ADD CONSTRAINT FK_Instancia_Inimigo_2
+    FOREIGN KEY (id_inimigo)
+    REFERENCES Inimigo (id_inimigo);
+ 
+ALTER TABLE Instancia_Inimigo ADD CONSTRAINT FK_Instancia_Inimigo_3
+    FOREIGN KEY (id_grupo)
+    REFERENCES Grupo_inimigo (id_grupo);
+
+ALTER TABLE Grupo_inimigo ADD CONSTRAINT FK_Grupo_inimigo_2
+    FOREIGN KEY (id_sala)
+    REFERENCES Sala (id_sala);
