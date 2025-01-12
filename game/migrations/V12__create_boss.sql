e9f2658c82ac117218af770304756947712bb78f
lucasdray <pedrolucasdourado@hotmail.com>
2025-01-12 16:27:55 -0300

    add create boss

---
+++ game/migrations/V12__create_boss.sql
@@ -0,0 +1,23 @@
+CREATE TABLE Boss (
+    id_boss INTEGER PRIMARY KEY,
+    id_sala INTEGER NOT NULL,
+    nome INTEGER NOT NULL,
+    nivel INTEGER NOT NULL,
+    xp_acumulado INTEGER NOT NULL,
+    hp_max INTEGER NOT NULL,
+    hp_atual INTEGER NOT NULL,
+    magia_max INTEGER NOT NULL,
+    magia_atual INTEGER NOT NULL,
+    velocidade INTEGER NOT NULL,
+    ataque_fisico_base INTEGER NOT NULL,
+    ataque_magico_base INTEGER NOT NULL,
+    dinheiro INTEGER,
+    fala_inicio VARCHAR,
+    fala_derrotar_player VARCHAR,
+    fala_derrotado VARCHAR,
+    fala_condicao VARCHAR
+);
+ 
+ALTER TABLE Boss ADD CONSTRAINT FK_Boss_2
+    FOREIGN KEY (id_sala)
+    REFERENCES Sala (id_sala);
