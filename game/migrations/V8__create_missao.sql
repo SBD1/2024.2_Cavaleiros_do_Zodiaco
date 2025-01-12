b3d6e5415b408a4a3cdd02303aa57bf178984bcf
lucasdray <pedrolucasdourado@hotmail.com>
2025-01-12 16:04:12 -0300

    create missao

---
+++ game/migrations/V8__create_missao.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Missao (
+    id_missao INTEGER PRIMARY KEY,
+    id_missao_anterior INTEGER,
+    item_necessario INTEGER NOT NULL,
+    nome INTEGER NOT NULL,
+    dialogo_inicial VARCHAR,
+    dialogo_durante VARCHAR,
+    dialogo_completa VARCHAR
+);
+ 
+ALTER TABLE Missao ADD CONSTRAINT FK_Missao_2
+    FOREIGN KEY (id_missao_anterior)
+    REFERENCES Missao (id_missao);
+ 
+ALTER TABLE Missao ADD CONSTRAINT FK_Missao_3
+    FOREIGN KEY (item_necessario)
+    REFERENCES Item_Missao (id_item);
