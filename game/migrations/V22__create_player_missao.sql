4fb28ff88128c0a706f9489a4d510a783e64e610
unknown <180145509@aluno.unb.br>
2025-01-12 19:12:09 -0300

    cria tabela player missao

---
+++ game/migrations/V22__create_player_missao.sql
@@ -0,0 +1,16 @@
+
+CREATE TYPE enum_status_missao as ENUM ('c','i','ni'); /* c = completo, i=iniciado,ni=não iniciado*/
+CREATE TABLE Player_Missao (
+    id_player INTEGER,
+    id_missao INTEGER,
+    status_missao enum_status_missao NOT NULL,
+    PRIMARY KEY (id_player, id_missao)
+);
+ 
+ALTER TABLE Player_Missao ADD CONSTRAINT FK_Player_Missao_2
+    FOREIGN KEY (id_missao)
+    REFERENCES Missao (id_missao);
+ 
+ALTER TABLE Player_Missao ADD CONSTRAINT FK_Player_Missao_3
+    FOREIGN KEY (id_player)
+    REFERENCES Player (id_player);
