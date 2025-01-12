272cc6adc0691672c96094f3d91a218061ff78f7
lucasdray <pedrolucasdourado@hotmail.com>
2025-01-12 16:37:41 -0300

    create inimigo

---
+++ game/migrations/V13__create_inimigo.sql
@@ -0,0 +1,23 @@
+CREATE TABLE Inimigo (
+    id_inimigo INTEGER PRIMARY KEY,
+    id_classe INTEGER NOT NULL,
+    id_elemento INTEGER NOT NULL,
+    nome VARCHAR NOT NULL,
+    nivel INTEGER NOT NULL,
+    xp_acumulado INTEGER NOT NULL,
+    hp_max INTEGER NOT NULL,
+    magia_max INTEGER NOT NULL,
+    velocidade INTEGER NOT NULL,
+    ataque_fisico_base INTEGER NOT NULL,
+    ataque_magico_base INTEGER NOT NULL,
+    dinheiro INTEGER,
+    fala_inicio VARCHAR
+);
+ 
+ALTER TABLE Inimigo ADD CONSTRAINT FK_Inimigo_2
+    FOREIGN KEY (id_elemento)
+    REFERENCES Elemento (id_elemento);
+ 
+ALTER TABLE Inimigo ADD CONSTRAINT FK_Inimigo_3
+    FOREIGN KEY (id_classe)
+    REFERENCES Classe (id_classe);
