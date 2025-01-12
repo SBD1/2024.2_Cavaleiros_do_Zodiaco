e830d996bb1284b511f2fea08d3dd3adf022a29d
lucasdray <pedrolucasdourado@hotmail.com>
2025-01-12 16:45:23 -0300

    create inventario

---
+++ game/migrations/V16__create_inventario.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Inventario (
+    id_player INTEGER PRIMARY KEY,
+    dinheiro INTEGER
+);
+ 
+ALTER TABLE Inventario ADD CONSTRAINT FK_Inventario_1
+    FOREIGN KEY (id_player)
+    REFERENCES Player (id_player);
