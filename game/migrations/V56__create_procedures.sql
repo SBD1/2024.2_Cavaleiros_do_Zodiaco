eb0f34e472b6ad0346b37f4a4b7525d0ea14356a
lucasdray <pedrolucasdourado@hotmail.com>
2025-02-01 18:36:55 -0300

    add get_missoes_cursor

--- game/migrations/V56__create_procedures.sql
+++ game/migrations/V56__create_procedures.sql
@@ -7,3 +7,33 @@ BEGIN
         WHERE id_player = p_id_player;
 END;
 $$;
+
+CREATE OR REPLACE PROCEDURE get_missoes_cursor(IN p_id_player INT, INOUT cur REFCURSOR)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    OPEN cur FOR
+        SELECT m.nome, m.dialogo_durante, m.dialogo_completa, pm.status_missao, im.nome, s.nome, c.nome, saga.nome FROM Player_missao as pm
+        JOIN
+        Missao AS m
+        ON pm.id_missao = m.id_missao
+        JOIN
+        Item_missao AS im
+        ON m.item_necessario = im.id_item
+        JOIN
+        Boss as b
+        ON b.id_item_missao = im.id_item
+        JOIN
+        Sala AS s 
+        ON s.id_sala = b.id_sala
+        JOIN
+        Casa AS c 
+        ON c.id_casa = s.id_casa
+        JOIN
+        Saga as saga 
+        ON saga.id_saga = c.id_saga
+        WHERE pm.id_player = p_id_player 
+        AND (status_missao = 'i' OR status_missao = 'c');
+END;
+$$;
+
