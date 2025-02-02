cea8ed81fa7cc8f45b8847856370a5d37acbcdf7
lucasdray <pedrolucasdourado@hotmail.com>
2025-02-02 12:03:58 -0300

    add: emojis em cada opções

--- game/src/usecases/obter_acoes_disponiveis.py
+++ game/src/usecases/obter_acoes_disponiveis.py
@@ -14,43 +14,35 @@ from .interagir_npc_quest import interagir_npc_quest
 
 def obter_acoes_disponiveis(jogador_id):
 
-
     # Opções comuns a todos os jogadores
     opcoes = [
-        ("[bold yellow]Ver Salas Disponíveis [/bold yellow]", ver_salas_disponiveis),
-        ("[bold green]Mudar de Sala[/bold green]", mudar_de_sala),
-        ("[bold purple]Ver Mapa[/bold purple] 🗺", ver_mapa),
-        ("[bold cyan]Ver Inventário[/bold cyan] 🎒", ver_inventario),
-        ("[bold blue]Ver Grupo[/bold blue] ⚔️", ver_grupo),
-        ("[bold red]Trocar Cavaleiro na Party[/bold red] 🔄", trocar_cavaleiro),
-        ("[light_pink4]Ver Missões[/light_pink4] ", ver_missoes)
+        ("[bold yellow]🛡️ Ver Salas Disponíveis [/bold yellow]", ver_salas_disponiveis),
+        ("[bold green]🚪 Mudar de Sala[/bold green]", mudar_de_sala),
+        ("[bold purple]🗺️ Ver Mapa[/bold purple]", ver_mapa),
+        ("[bold cyan]🎒 Ver Inventário[/bold cyan]", ver_inventario),
+        ("[bold blue]⚔️ Ver Grupo[/bold blue]", ver_grupo),
+        ("[bold red]🔄 Trocar Cavaleiro na Party[/bold red]", trocar_cavaleiro),
+        ("[light_pink4]📜 Ver Missões[/light_pink4]", ver_missoes),
     ]
 
     # Opções que variam conforme o estado do jogador
     if verificar_se_esta_na_saga_orfanato(jogador_id):
-        opcoes.append(("[bold blue]Mudar de Saga[/bold blue]", mudar_saga))
+        opcoes.append(("[bold blue]📖 Mudar de Saga[/bold blue]", mudar_saga))
     else:
-        opcoes.append(("Mudar de Casa", mudar_casa))
-        opcoes.append(("[bold cyan]Voltar para o Orfanato[/bold cyan]", mudar_para_orfanato))
+        opcoes.append(("[bold magenta]🏠 Mudar de Casa[/bold magenta]", mudar_casa))
+        opcoes.append(("[bold cyan]🌟 Voltar para o Orfanato[/bold cyan]", mudar_para_orfanato))
 
     check_npc = verificar_npc_na_sala(jogador_id)
 
     if check_npc == "Ferreiro":
-        opcoes.insert(0, ("[orange4]Falar com Mu[/orange4]", None))
+        opcoes.insert(0, ("[orange4]🔨 Falar com Mu (Ferreiro)[/orange4]", None))
 
     elif check_npc == "Missao":
-        opcoes.insert(0, ("[sea_green1]Falar com Saori Kido[/sea_green1]", interagir_npc_quest))
+        opcoes.insert(0, ("[sea_green1]💬 Falar com Saori Kido (Missões)[/sea_green1]", interagir_npc_quest))
 
     elif check_npc == "Mercador":
-        opcoes.insert(0, ("[chartreuse2]Falar com Jabu[/chartreuse2]", None))
-        
+        opcoes.insert(0, ("[chartreuse2]💰 Falar com Jabu (Mercador)[/chartreuse2]", None))
 
-    opcoes.append(("[bold red]Sair do Menu de Ações[/bold red]", None))
-
-    # 7 listar missões -> escolher uma missao pra saber as info ou voltar pro menu principal
-    # falar com npc
-    # lutar -> caso haja inimigo na sala aparecer a opção
-    # ver grupo -> trocar cavaleiros -> lista todos os cavaleiros
-    # 
+    opcoes.append(("[bold red]❌ Sair do Menu de Ações[/bold red]", None))
 
     return opcoes
